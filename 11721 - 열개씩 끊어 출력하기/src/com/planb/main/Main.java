6091dc56332f85db476b507e604a0a1394951e84
JoMingyu <city7310@naver.com>
2017-03-27 13:31:55 +0900

    11721 success

--- 11721 - 열개씩 끊어 출력하기/src/com/planb/main/Main.java
+++ 11721 - 열개씩 끊어 출력하기/src/com/planb/main/Main.java
@@ -1,5 +1,23 @@
 package com.planb.main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String input = scanner.nextLine();
+		scanner.close();
+		char[] charInput = input.toCharArray();
+		
+		int cnt = 0;
+		for(char c: charInput) {
+			cnt++;
+			System.out.print(c);
+			if(cnt == 10) {
+				System.out.println();
+				cnt = 0;
+			}
+		}
+	}
 }
