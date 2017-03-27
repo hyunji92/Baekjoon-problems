b3774266ef70a5f924fbf81124daa8f30f1bc24e
JoMingyu <city7310@naver.com>
2017-03-27 14:26:30 +0900

    1001 success

--- 1001 - A-B/src/com/planb/main/Main.java
+++ 1001 - A-B/src/com/planb/main/Main.java
@@ -1,10 +1,15 @@
 package com.planb.main;
 
+import java.util.Scanner;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(a - b);
 	}
-
 }
